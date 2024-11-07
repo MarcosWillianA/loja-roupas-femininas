93183de58ddee89a78153dfcbd2ccf84a624f05f
Marcos Willian <marcoswillianl1987@gmail.com>
2024-11-07 12:22:59 -0300

    busca dinamica
    Busca dinâmica ao digitar feita.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
 const buscador = document.querySelector('#buscador');
-const botaoBuscar = document.querySelector('#botao-buscar');
 const areaProdutos = document.querySelector('#area-produtos');
 
 class Produto {
@@ -14,45 +13,52 @@ class Produto {
         const cartaoProdutos = document.createElement('div');
         cartaoProdutos.classList.add('cartao-produtos');
         cartaoProdutos.innerHTML = `
-            <img src="${this.imagem}" alt="${this.nome}"</img>
+            <img src="${this.imagem}" alt="${this.nome}" />
             <h3>${this.nome}</h3>
             <p>${this.preco}</p>
         `;
+
+        return cartaoProdutos;
     }
 }
 
 class Estante {
-    constructor(produto) {
-        this.produto = produto;
+    constructor(arrayProdutos) {
+        this.arrayProdutos = arrayProdutos;
     }
 
-    produto = [];
-
-    exibirProdutos(produto) {
+    exibirProdutos(arrayProdutos) {
         areaProdutos.innerHTML = '';
-        areaProdutos.appendChild(produto.cartaoProduto()); 
+        if (arrayProdutos.length > 0) {
+            arrayProdutos.forEach(produto => {
+                areaProdutos.appendChild(produto.cartaoProduto());
+            });
+        } else {
+            areaProdutos.innerHTML = 'Nenhum produto encontrado.';
+        }
     }
 
-    buscarProduto(busca) {
-        botaoBuscar.addEventListener('click', () => {
-            busca = buscador.value;
-            let itensBuscados = listaDeProdutos.filter(produto => produto.nome.toLowerCase().includes(busca));
+    buscarProduto() {
+        buscador.addEventListener('input', () => {
+            const busca = buscador.value.toLowerCase();
+            const itensBuscados = this.arrayProdutos.filter(produto => produto.nome.toLowerCase().includes(busca));
             this.exibirProdutos(itensBuscados);
-            if (itensBuscados.length === 0) {
-                areaProdutos.innerHTML = 'Nenhum produto foi encontrado';
-            }
         })
     }
 }
 
+const blusaLaranja = new Produto('blusa laranja', 'blusa', 'img/blusa-laranja.png', 79.99);
+const blusaVerde = new Produto('blusa verde', 'blusa', 'img/blusa-verde001.png', 79.99);
+const botaCouroMarrom = new Produto('bota de couro marrom', 'bota', 'img/bota-couro-marrom001.png', 189.99);
+const botaCouroPreta = new Produto('bota de couro preta', 'bota', 'img/bota-couro-preta001.png', 189.99);
+const saiaAzul = new Produto('saia azul', 'saia', 'img/saia-azul001.png', 99.99);
+const saiaRosa = new Produto('saia rosa', 'saia', 'img/saia-rosa001.png', 99.99);
+const vestidoAzul = new Produto('vestido azul', 'vestido', 'img/vestido-azul001.png', 139.99);
+const vestidoAmarelo = new Produto('vestido amarelo', 'vestido', 'img/vestido-longo-amarelo001.png', 139.99);
+const vestidoVermelho = new Produto('vestido vermelho', 'vestido', 'img/vestido-vermelho001.png', 139.99);
 
-    const blusaLaranja = new Produto('blusa laranja', 'blusa', 'img/blusa-laranja.png', 79.99);
-    const blusaVerde = new Produto('blusa verde', 'blusa', 'img/blusa-verde001.png', 79.99);
-    const botaCouroMarrom = new Produto('bota de couro marrom', 'bota', 'img/bota-couro-marrom001.png', 189.99);
-    const botaCouroPreta = new Produto('bota de couro preta', 'bota', 'img/bota-couro-preta0001.png', 189.99);
-    const saiaAzul = new Produto('saia azul', 'saia', 'img/saia-azul001.png', 99.99);
-    const saiaRosa = new Produto('saia rosa', 'saia', 'img/saia-rosa001.png', 99.99);
-    const vestidoAzul = new Produto('vestido azul', 'vestido', 'img/vestido-azul001.png', 139.99);
-    const vestidoAmarelo = new Produto('vestido amarelo', 'vestido', 'img/vestido-longo-amarelo001.png', 139.99);
-    const vestidoVermelho = new Produto('vestido vermelho', 'vestido', 'img/vestido-vermelho001.png', 139.99);
+const listaDeProdutos = [blusaLaranja, blusaVerde, botaCouroMarrom, botaCouroPreta, saiaAzul, saiaRosa, vestidoAzul, vestidoAmarelo, vestidoVermelho];
 
+const estante = new Estante (listaDeProdutos);
+estante.exibirProdutos(listaDeProdutos);
+estante.buscarProduto();
